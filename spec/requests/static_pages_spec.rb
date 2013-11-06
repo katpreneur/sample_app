b9331ddde72ab3841963d4a9afba052c6ba90bb6
Katerina <katekson@gmail.com>
2013-11-06 06:06:34 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,51 @@
-require 'capybara/rspec'
+require 'spec_helper'
 
 
 
 describe "Static Pages" do
+
 	describe "Home page" do 
-    it "should have the content 'Sample App'" do
-     visit '/static_pages/home'
-     expect(page).to have_content('Sample App')
-    end
-  end
+
+    	it "should have the content 'Sample App'" do
+    		 visit '/static_pages/home'
+     		expect(page).to have_content('Sample App')
+   		end
+
+   		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
+
+  	end
+
 
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+
+		it "should have the content 'Sample App'" do
 			visit '/static_pages/help'
-			expect(page).to have_content('Help')
+			expect(page).to have_content('Sample App')
+		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
-end
+
+
+
+	describe "About Us" do
+
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('Sample App')
+		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		end
+	end
+end 
+
